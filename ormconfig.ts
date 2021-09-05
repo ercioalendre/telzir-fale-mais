e0fd3da3273d8c1a9edb85a7d2c4b0fbc0688a9d
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-04 22:05:13 -0300

    ormconfig file created

---
+++ ormconfig.ts
@@ -0,0 +1,30 @@
+const appMode = process.env.APP_MODE === "prod" ? "build" : "src";
+const dropSchema = process.env.TYPEORM_DROP_SCHEMA === "true" ? true : false;
+const migrationsRun =
+  process.env.TYPEORM_MIGRATIONS_RUN === "true" ? true : false;
+const logging = process.env.TYPEORM_LOGGING === "true" ? true : false;
+const synchronize = process.env.TYPEORM_SYNCHRONIZE === "true" ? true : false;
+
+module.exports = {
+  type: process.env.TYPEORM_DB_TYPE,
+  host: process.env.TYPEORM_DB_HOST,
+  port: process.env.TYPEORM_DB_PORT,
+  username: process.env.TYPEORM_DB_USERNAME,
+  password: process.env.TYPEORM_DB_PASSWORD,
+  database: process.env.TYPEORM_DB_DATABASE,
+  synchronize,
+
+  /* Log settings */
+  logging,
+  logger: "file",
+
+  /* Set true for testings */
+  dropSchema,
+  migrationsRun,
+
+  entities: [`./${appMode}/modules/**/typeorm/entities/*.{js,ts}`],
+  migrations: [`./${appMode}/shared/typeorm/migrations/*.{js,ts}`],
+  cli: {
+    migrationsDir: `./${appMode}/shared/typeorm/migrations`,
+  },
+};
