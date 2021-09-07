440ec0a086c230af0aa7080dab77e786cf230a36
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-06 23:08:51 -0300

    routes file updated

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -8,15 +8,8 @@ const router = express.Router();
 
 router.get("/", isUserLoggedIn, (req, res) => res.redirect("/login"));
 router.get("/signup", isUserLoggedIn, UsersController.signup);
-
 router.post("/signup", checkNewUserForm, UsersController.create);
-router.get("/login", isUserLoggedIn, (req, res) =>
-  res.render("main", {
-    page: "login-block",
-    msgType: "",
-    msgContent: "",
-  }),
-);
+router.get("/login", isUserLoggedIn, UsersController.login);
 router.post("/login/create-session", UsersController.createUserSession);
 router.get("/logout", UsersController.logout);
 router.get("/my-account", isAuthenticated, UsersController.index);
