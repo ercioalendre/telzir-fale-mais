30ceb15cef84d2ca18ab92c00c9c63499a4db6de
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-08 14:06:40 -0300

    middleware to check login user form created

---
+++ src/shared/http/middlewares/checkLoginUserForm.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function checkNewUserForm(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const { phone, password } = req.body;
+  const formData = { phone, password };
+  const inputError: string[] = [];
+  Object.entries(formData).forEach(([key, val]) => {
+    if (!val || val == "" || val == undefined) {
+      let message;
+
+      if (key === "password") {
+        message = "A senha inserida é inválida";
+      }
+      if (key === "phone") {
+        message = "O número de telefone inserido é inválido";
+      }
+
+      inputError.push(key);
+
+      message =
+        inputError.length == 1
+          ? message
+          : "Um ou mais valores inseridos são inválidos.";
+
+      res.locals.message = {
+        msgType: "error",
+        msgContent: message,
+        inputError,
+      };
+    }
+  });
+  res.locals.formData = {
+    phone,
+  };
+  next();
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import UsersController from "@modules/users/controllers/Users.controller";
 import isAuthenticated from "@middlewares/isAuthenticated";
 import isUserLoggedIn from "@middlewares/isUserLoggedIn";
 import checkNewUserForm from "@middlewares/checkNewUserForm";
+import checkLoginUserForm from "@middlewares/checkLoginUserForm";
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.get("/", isUserLoggedIn, (req, res) => res.redirect("/login"));
 router.get("/signup", isUserLoggedIn, UsersController.signup);
 router.post("/signup", checkNewUserForm, UsersController.create);
 router.get("/login", isUserLoggedIn, UsersController.login);
-router.post("/login", UsersController.createUserSession);
+router.post("/login", checkLoginUserForm, UsersController.createUserSession);
 router.get("/logout", UsersController.logout);
 router.get("/my-account", isAuthenticated, UsersController.index);
 
