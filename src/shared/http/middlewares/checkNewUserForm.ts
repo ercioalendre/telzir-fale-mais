1871d92aaefb35727f8a79076a35f76ec3560396
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-08 14:05:58 -0300

    refactor: middleware to check new users registration form was refactored

--- src/shared/http/middlewares/checkNewUserForm.ts
+++ src/shared/http/middlewares/checkNewUserForm.ts
@@ -8,39 +8,64 @@ export default function checkNewUserForm(
   const { name, email, phone, password } = req.body;
   const formData = { name, email, phone, password };
   const inputError: string[] = [];
-  Object.entries(formData).forEach(([key, val]) => {
-    if (!val || val == "" || val == undefined) {
-      let message;
+  let message = "";
+  const parsedName = name.toUpperCase();
+  const regName = new RegExp("^[a-zA-Z]{2,}(?: [a-zA-Z]+){1,}$");
+  const regEmail = new RegExp("^[^@\\s]+@[^@\\s]+\\.+[^@\\s]{2,}$");
+  const regPhone = new RegExp(
+    "^(?:\\()[0-9]{2}(?:\\))\\s[0-9]{4,5}(?:-)[0-9]{4}$",
+  );
 
-      if (key === "password") {
-        message = "A senha inserida é inválida";
-      }
-      if (key === "phone") {
-        message = "O número de telefone inserido é inválido";
+  Object.entries(formData).forEach(([key, val]) => {
+    if (val) {
+      if (key === "name") {
+        if (!regName.test(parsedName)) {
+          message = "O nome completo inserido é inválido";
+          inputError.push(key);
+        }
       }
       if (key === "email") {
-        message = "O endereço de e-mail inserido é inválido";
+        if (!regEmail.test(email)) {
+          message = "O endereço de e-mail inserido é inválido";
+          inputError.push(key);
+        }
       }
-      if (key === "name") {
-        message = "O nome completo inserido é inválido";
+      if (key === "phone") {
+        if (!regPhone.test(phone)) {
+          message = "O número de telefone inserido é inválido";
+          inputError.push(key);
+        }
       }
-
+      if (key === "password") {
+        if (String(val).length < 6) {
+          message = "A senha deve conter seis ou mais caracteres.";
+          inputError.push(key);
+        }
+      }
+    } else {
       inputError.push(key);
-
-      message =
-        inputError.length == 1
-          ? message
-          : "Um ou mais valores inseridos são inválidos.";
-
-      res.locals.message = {
-        msgType: "error",
-        msgContent: message,
-        inputError,
-      };
+      let keyName;
+      if (key === "name") keyName = "nome";
+      if (key === "password") keyName = "senha";
+      if (key === "phone") keyName = "telefone";
+      if (key === "password") keyName = "senha";
+      message = `O campo ${keyName} é obrigatório.`;
     }
+
+    message =
+      inputError.length <= 1
+        ? message
+        : "Um ou mais valores inseridos são inválidos.";
+
+    res.locals.message = {
+      msgType: "error",
+      msgContent: message,
+      inputError,
+    };
   });
+
   res.locals.formData = {
-    name,
+    name: parsedName,
     email,
     phone,
     password,
