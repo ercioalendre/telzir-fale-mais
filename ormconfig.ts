cd3596030401914322a93d8a8e9208e3f66d2356
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-14 16:02:05 -0300

    testing settings updated

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,8 +1,14 @@
+import { join } from "path";
+
 const appMode = process.env.APP_MODE === "prod" ? "build" : "src";
 const dropSchema = process.env.TYPEORM_DROP_SCHEMA === "true" ? true : false;
 const migrationsRun = process.env.TYPEORM_MIGRATIONS_RUN === "true" ? true : false;
 const logging = process.env.TYPEORM_LOGGING === "true" ? true : false;
 const synchronize = process.env.TYPEORM_SYNCHRONIZE === "true" ? true : false;
+const database =
+  process.env.APP_MODE === "test"
+    ? "./src/__tests__/database/falemais.sqlite"
+    : process.env.TYPEORM_DB_DATABASE;
 
 module.exports = {
   type: process.env.TYPEORM_DB_TYPE,
@@ -10,7 +16,7 @@ module.exports = {
   port: process.env.TYPEORM_DB_PORT,
   username: process.env.TYPEORM_DB_USERNAME,
   password: process.env.TYPEORM_DB_PASSWORD,
-  database: process.env.TYPEORM_DB_DATABASE,
+  database,
   synchronize,
 
   /* Log settings */
@@ -21,8 +27,8 @@ module.exports = {
   dropSchema,
   migrationsRun,
 
-  entities: [`./${appMode}/modules/**/typeorm/entities/*.{js,ts}`],
-  migrations: [`./${appMode}/shared/typeorm/migrations/*.{js,ts}`],
+  entities: [join(`./${appMode}/modules/`, "**", "/typeorm/entities/", "*.{ts,js}")],
+  migrations: [join(`./${appMode}/shared/`, "/typeorm/migrations/", "*.{ts,js}")],
   cli: {
     migrationsDir: `./${appMode}/shared/typeorm/migrations`,
   },
