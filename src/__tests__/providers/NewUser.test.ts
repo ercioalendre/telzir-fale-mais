73049d989367781933ccde6af96ae949157771fe
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-15 11:30:21 -0300

    providers folder created to help with tests

---
+++ src/__tests__/providers/NewUser.test.ts
@@ -0,0 +1,26 @@
+import app from "@shared/http/app";
+import supertest, { Response } from "supertest";
+
+interface IUser {
+  name?: string;
+  email?: string;
+  phone?: string;
+  password?: string;
+}
+
+export default async function createNewUser({
+  name = "USUARIO TESTE",
+  email = "usuario@teste.com.br",
+  phone = "(11) 11111-1111",
+  password = "teste123",
+}: IUser): Promise<Response> {
+  const newUser = {
+    name,
+    email,
+    phone,
+    password,
+  };
+
+  const doCreate = await Promise.resolve(supertest(app).post("/signup").send(newUser));
+  return doCreate;
+}
